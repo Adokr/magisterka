67c91e0f5bf253246c0c54a7cc805b3a786ecc8b
Adokr <aj.okrasinsk@student.uw.edu.pl>
2025-04-28 18:32:26 +0200

    despaghettifikacja

--- demo.py
+++ demo.py
@@ -2,9 +2,11 @@ from combo.predict import COMBO
 #import torch
 
 #print(f"TORCH: {torch.cuda.is_available()}")
+
+TAGS = {'advcl', 'ccomp', 'xcomp', 'csubj', 'acl', 'conj'}
+
 # Download Polish model. Change cuda value to use GPU
 nlp = COMBO.from_pretrained("polish-herbert-base-ud213", cuda_device=-1)
-
 sentence = nlp("Padał deszcz, więc wróciłem do domu, a potem zjadłem placki i naleśniki.")
 
 print("{:15} {:15} {:10} {:10} {:10}".format('TOKEN', 'LEMMA', 'UPOS', 'HEAD', 'DEPREL'))
@@ -12,11 +14,7 @@ for token in sentence[0].tokens:
     print("{:15} {:15} {:10} {:10} {:10}".format(token.text, token.lemma, token.upostag, token.head, token.deprel))
 
 def find_governors(sentence):
-    for token in sentence[0].tokens:
-        if token.deprel == 'root':
-            root_id = token.idx
-            governors_ids = [root_id]
-            break
+    governors_ids = [token.idx for token in sentence[0].tokens if token.deprel=='root']
     assert len(governors_ids) != 0, "no root found"
 
     for token in sentence[0].tokens:
@@ -24,28 +22,31 @@ def find_governors(sentence):
             governors_ids.append(token.idx)
     return governors_ids
 
+
 def get_dependencies(sentence, root_id, stop_token_id):
     dependents = [root_id]
-    heads = []
-    for token in sentence[0].tokens:
-        heads.append(token.head)
-    heads = set(heads)
+    heads = set([token.head for token in sentence[0].tokens])
 
-    skip = False
-    for i, token in enumerate(sentence[0].tokens):
-        if not skip:
-            if token.idx != root_id and token.head == root_id and token.idx not in stop_token_id:
-                if token.upostag not in ["SCONJ", "CCONJ"]:
-                    dependents.append(token.idx)
-                    if token.idx in heads and token.upostag in ["VERB", "NOUN"]:
-                        dependents.extend(get_dependencies(sentence, token.idx, []))
-                elif not (token.deprel == "cc" and  sentence[0].tokens[i+1].deprel =="advmod"):
-                    dependents.append(token.idx)
-                else:
-                    skip = True
-        else:
-            skip = False
+    skip_next = False
+    tokens = sentence[0].tokens
 
+    for i, token in enumerate(tokens):
+        if skip_next:
+            skip_next = False
+            continue
+        
+        if token.idx == root_id or token.idx in stop_token_id:
+            continue
+
+        if token.head == root_id:
+            if token.upostag not in ["SCONJ", "CCONJ"]:
+                dependents.append(token.idx)
+                if token.idx in heads and token.upostag in ["VERB", "NOUN"]:
+                    dependents.extend(get_dependencies(sentence, token.idx, []))
+            elif not (token.deprel == "cc" and sentence[0].tokens[i+1].deprel =="advmod"):
+                dependents.append(token.idx)
+            else:
+                skip_next = True
     return set(dependents)        
 
 def get_subtrees(sentence, governors_ids):
@@ -59,26 +60,24 @@ def get_subtrees(sentence, governors_ids):
     return subtrees 
 
 def visualize(sentence, ids, unit_count):
-    result = f"Unit {unit_count}:"
+    result = [f"Unit {unit_count}:"]
     for el in sorted(ids):
         postag = sentence[0].tokens[el-1].upostag
-        head = sentence[0].tokens[el-1].head
         if  postag not in ["AUX", "PUNCT", "SCONJ"]:
-                result += " "
+            result.append(" ")
         if  postag not in ["PUNCT", "SCONJ"]:
-            result += sentence[0].tokens[el-1].text
-    return result.strip()
+            result.append(sentence[0].tokens[el-1].text)
+    return ''.join(result).strip()
 
-#print(visualize(sentence, get_dependencies(sentence, 1, [5])))
-#print(visualize(sentence, get_dependencies(sentence, 5, [])))
+governors_ids = [token.idx for token in sentence[0].tokens if token.deprel=='root']
+assert len(governors_ids) != 0, "no root found"
 
+for token in sentence[0].tokens:
+    if token.head in governors_ids and token.deprel in TAGS:
+        governors_ids.append(token.idx)
 
-govs = sorted(find_governors(sentence))
-#print(govs)
 print("##########")
-for i, gov in enumerate(govs):
-    #print(get_dependencies(sentence, gov, govs))
-    print(visualize(sentence, get_dependencies(sentence, gov, govs), i+1))
+for i, gov in enumerate(sorted(governors_ids)):
+    print(visualize(sentence, get_dependencies(sentence, gov, governors_ids), i+1))
 
-#print(get_subtrees(sentence, find_governors(sentence)))
 
