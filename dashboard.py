a1b2e5e3bc80d0b975029333c6368fb74ce5300f
Adokr <aj.okrasinsk@student.uw.edu.pl>
2025-05-05 10:44:27 +0200

    container

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,9 @@ import streamlit as st
 from spacy_streamlit import visualize_spans
 import demo
 
-st.text_input("Enter a sentence for segmentation", key="sentence")
-docs, options = demo.run_segmentation(st.session_state.sentence)
 
-visualize_spans(docs, spans_key="sc", displacy_options=options)
+if __name__ == "__main__":
+    st.text_input("Enter a sentence for segmentation", key="sentence")
+    docs, options = demo.run_segmentation(st.session_state.sentence)
+
+    visualize_spans(docs, spans_key="sc", displacy_options=options)
