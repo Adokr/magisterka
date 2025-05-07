f645f36d0ec30d6a62a6305ca729d8d0c85d887d
Adekie <118201144+Adokr@users.noreply.github.com>
2025-05-07 13:21:11 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,13 +10,18 @@ if __name__ == "__main__":
     nlp_blank = spacy.blank("pl")
 
     st.title("Automatic Discourse Segmentation")
-    
-   
+
+    if "text_input" not in st.session_state:
+        st.session_state.text_input = ""
+       
     user_input = st.text_area("Enter a sentence to segment", key="text_input", height=100)
 
-    if user_input.strip():
-        docs, options = demo.run_segmentation(user_input, combo, nlp_blank)
-        visualize_spans(docs, spans_key="sc", displacy_options=options)
+    if st.button("Segment"):
+        if user_input.strip():
+            docs, options = demo.run_segmentation(user_input, combo, nlp_blank)
+            visualize_spans(docs, spans_key="sc", displacy_options=options)
+
+            st.session_state.text_input = ""
         #st.success("Analysis complete. You can enter new text above.")
             #st.text_input("Enter another sentence for segmentation", key="sentence1")
 
